dcf36a936c87de68d20b76323f88615df0a23141
Eugene Yan <enegue84@gmail.com>
2023-12-09 20:11:38 +0800

    initial styles for both Header and Sidebar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: lightgray;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
 const Header = () => {
-  return <header>Header here!</header>;
+  return <StyledHeader>Header here!</StyledHeader>;
 };
 
 export default Header;
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  background-color: lightskyblue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
 const Sidebar = () => {
-  return <aside>sidebar!</aside>;
+  return <StyledSidebar>sidebar!</StyledSidebar>;
 };
 
 export default Sidebar;

80fc8e1646e02e78c28c27533071be27a1054757
Eugene Yan <enegue84@gmail.com>
2023-12-09 18:21:30 +0800

    testing styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import GlobalStyles from './styles/GlobalStyles';
+import Input from './ui/Input';
+import Button from './ui/Button';
+
 const App = () => {
-  return <div>App.jsx here!</div>;
+  const handleClick = () => {
+    alert(`Button clicked!`);
+  };
+
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <Button onClick={handleClick}>Test Button</Button>
+        <Input placeholder="No. of guests" />
+      </div>
+    </>
+  );
 };
 
 export default App;

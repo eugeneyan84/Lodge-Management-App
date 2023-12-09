bff304849fbadb9faa67d2196f316d92228028c5
Eugene Yan <enegue84@gmail.com>
2023-12-09 20:19:55 +0800

    swapped out debugging colours

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Header from './Header';
 import styled from 'styled-components';
 
 const Main = styled.main`
-  background-color: lightcoral;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 

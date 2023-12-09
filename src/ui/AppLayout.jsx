a1748749ad6fe62b2f66e0a0d1ad8bdab9f7f35a
Eugene Yan <enegue84@gmail.com>
2023-12-09 20:17:18 +0800

    styled the AppLayout with grid, and updated Sidebar style to span all rows

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,15 +8,22 @@ const Main = styled.main`
   padding: 4rem 4.8rem 6.4rem;
 `;
 
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
 const AppLayout = () => {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledAppLayout>
   );
 };
 
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -4,6 +4,8 @@ const StyledSidebar = styled.aside`
   background-color: lightskyblue;
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
 `;
 
 const Sidebar = () => {

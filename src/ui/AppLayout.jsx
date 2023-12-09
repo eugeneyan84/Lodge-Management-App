ea1b0a1548714cf281791952a8bdc11c367de220
Eugene Yan <enegue84@gmail.com>
2023-12-09 20:07:17 +0800

    integrated Header and Sidebar into AppLayout, defined Main-style

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  background-color: lightcoral;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 const AppLayout = () => {
   return (
     <div>
-      <p>App Layout!</p>
-      <Outlet />
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
     </div>
   );
 };

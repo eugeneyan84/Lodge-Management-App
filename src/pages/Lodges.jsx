6b488399577721809c875ab3aa481c750de67b9c
Eugene Yan <enegue84@gmail.com>
2023-12-11 10:55:46 +0800

    integrated LodgeTable into Lodges page

--- src/pages/Lodges.jsx
+++ src/pages/Lodges.jsx
@@ -1,20 +1,18 @@
 import Row from '../ui/Row';
 import Heading from '../ui/Heading';
-import { useEffect } from 'react';
-import { getLodges } from '../services/apiLodges';
+import LodgeTable from '../features/lodges/LodgeTable';
 
 const Lodges = () => {
-  useEffect(() => {
-    getLodges().then((data) => {
-      console.log(data);
-    });
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All Lodges</Heading>
-      <p>Test Lodges</p>
-      <img src="https://qeonbuukmudyccasecjs.supabase.co/storage/v1/object/public/lodge-images/lodge-001.jpg" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All Lodges</Heading>
+        <p>Test Lodges</p>
+      </Row>
+      <Row>
+        <LodgeTable />
+      </Row>
+    </>
   );
 };
 

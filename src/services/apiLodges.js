2fa1ccb0012c4ec8c43ea48bea18a240d3decfaa
Eugene Yan <enegue84@gmail.com>
2023-12-11 17:13:44 +0800

    API delete function

--- src/services/apiLodges.js
+++ src/services/apiLodges.js
@@ -10,3 +10,19 @@ export const getLodges = async () => {
 
   return data;
 };
+
+export const deleteLodge = async (id) => {
+  const { data, error } = await supabase.from('lodges').delete().eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Lodges could not be loaded');
+  }
+
+  if (error) {
+    console.error(error);
+    throw new Error('Lodge could not be deleted');
+  }
+
+  return data;
+};

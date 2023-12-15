49bcabc0f22eae4da3716070b6f6d51bbadd3364
Eugene Yan <enegue84@gmail.com>
2023-12-15 21:55:30 +0800

    implemented useUpdateSettings hook, and integrated it into UpdateSettingsForm

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
 import { useSettings } from './useSettings';
+import { useUpdateSettings } from './useUpdateSettings';
 
 const UpdateSettingsForm = () => {
   const {
@@ -16,6 +17,19 @@ const UpdateSettingsForm = () => {
     error,
   } = useSettings();
 
+  const { isUpdating, updateSettings } = useUpdateSettings();
+
+  const handleUpdate = (e, field) => {
+    const { value } = e.target;
+    console.log(value);
+
+    if (!value) {
+      return;
+    } else {
+      updateSettings({ [field]: value });
+    }
+  };
+
   if (isLoading) {
     return <Spinner />;
   }
@@ -23,16 +37,27 @@ const UpdateSettingsForm = () => {
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e, 'min_booking_length')}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, 'max_booking_length')}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, 'max_guests_per_booking')}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -40,6 +65,7 @@ const UpdateSettingsForm = () => {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, 'breakfast_price')}
         />
       </FormRow>
     </Form>

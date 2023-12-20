a762006e4eebebab1a924e931d639e5f2fcd0a44
Eugene Yan <enegue84@gmail.com>
2023-12-20 16:11:40 +0800

    pass in list of options to Filter

--- src/features/lodges/LodgeTableOps.jsx
+++ src/features/lodges/LodgeTableOps.jsx
@@ -4,7 +4,14 @@ import Filter from '../../ui/Filter';
 const LodgeTableOps = () => {
   return (
     <TableOps>
-      <Filter />
+      <Filter
+        filterField="discount"
+        options={[
+          { value: 'all', label: 'All' },
+          { value: 'no-discount', label: 'No discount' },
+          { value: 'with-discount', label: 'With discount' },
+        ]}
+      />
     </TableOps>
   );
 };

8eca41385e806726506d80adb1fea7c58658cda0
Eugene Yan <enegue84@gmail.com>
2023-12-20 12:29:06 +0800

    integrate LodgeTableOps into Lodges

--- src/pages/Lodges.jsx
+++ src/pages/Lodges.jsx
@@ -2,12 +2,14 @@ import Row from '../ui/Row';
 import Heading from '../ui/Heading';
 import LodgeTable from '../features/lodges/LodgeTable';
 import AddLodge from '../features/lodges/AddLodge';
+import LodgeTableOps from '../features/lodges/LodgeTableOps';
 
 const Lodges = () => {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All Lodges</Heading>
+        <LodgeTableOps />
       </Row>
       <Row>
         <LodgeTable />

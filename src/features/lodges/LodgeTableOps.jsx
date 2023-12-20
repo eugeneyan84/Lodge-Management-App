7f9b3210cf9cbe3c360935487f004985e6e3581a
Eugene Yan <eugene.yan@outlook.com>
2023-12-20 22:55:38 +0800

    define remaining sort options

--- src/features/lodges/LodgeTableOps.jsx
+++ src/features/lodges/LodgeTableOps.jsx
@@ -17,6 +17,16 @@ const LodgeTableOps = () => {
         options={[
           { value: 'name-asc', label: 'Sort by name (A-Z)' },
           { value: 'name-desc', label: 'Sort by name (Z-A)' },
+          { value: 'regular_price-asc', label: 'Sort by price (low to high)' },
+          { value: 'regular_price-desc', label: 'Sort by price (high to low)' },
+          {
+            value: 'max_capacity-asc',
+            label: 'Sort by capacity (low to high)',
+          },
+          {
+            value: 'max_capacity-desc',
+            label: 'Sort by capacity (high to low)',
+          },
         ]}
       />
     </TableOps>

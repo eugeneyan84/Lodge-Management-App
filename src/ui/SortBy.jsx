45158c8d3b6cf6b95adc69ec4171b4212c4fc631
Eugene Yan <eugene.yan@outlook.com>
2023-12-20 22:55:14 +0800

    implement sortBy and Select components

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,5 +1,24 @@
-const SortBy = () => {
-  return <div></div>;
+import { useSearchParams } from 'react-router-dom';
+import Select from './Select';
+
+const SortBy = ({ options }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const sortByValue = searchParams.get('sortBy') || '';
+
+  const handleSelectionChange = (e) => {
+    searchParams.set('sortBy', e.target.value);
+    setSearchParams(searchParams);
+  };
+
+  return (
+    <Select
+      options={options}
+      value={sortByValue}
+      type="white"
+      onChange={handleSelectionChange}
+    />
+  );
 };
 
 export default SortBy;

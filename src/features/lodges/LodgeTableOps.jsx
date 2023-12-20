d9b095adc858df03b38f14da75457ce34ddb0fc3
Eugene Yan <enegue84@gmail.com>
2023-12-20 12:28:44 +0800

    set up Filter, integrate Filter and TableOps into LodgeTableOps

---
+++ src/features/lodges/LodgeTableOps.jsx
@@ -0,0 +1,12 @@
+import TableOps from '../../ui/TableOps';
+import Filter from '../../ui/Filter';
+
+const LodgeTableOps = () => {
+  return (
+    <TableOps>
+      <Filter />
+    </TableOps>
+  );
+};
+
+export default LodgeTableOps;

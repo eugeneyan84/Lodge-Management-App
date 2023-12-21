bab811e0814469a68227b75b8b0d568bd503ef77
Eugene Yan <enegue84@gmail.com>
2023-12-21 17:17:34 +0800

    built bookings table with associated row component and useBookings hook, and integrated into Bookings page

---
+++ src/features/bookings/BookingTable.jsx
@@ -0,0 +1,41 @@
+import BookingRow from './BookingRow';
+import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
+import Empty from '../../ui/Empty';
+import { useBookings } from './useBookings';
+import Spinner from '../../ui/Spinner';
+
+const BookingTable = () => {
+  const { isLoading, bookings, error } = useBookings();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!bookings.length) {
+    return <Empty resource="bookings" />;
+  }
+
+  const renderBookingRecords = (booking) => {
+    return <BookingRow key={booking.id} booking={booking} />;
+  };
+
+  return (
+    <Menus>
+      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+        <Table.Header>
+          <div>Lodge</div>
+          <div>Guest</div>
+          <div>Dates</div>
+          <div>Status</div>
+          <div>Amount</div>
+          <div></div>
+        </Table.Header>
+
+        {<Table.Body data={bookings} renderFn={renderBookingRecords} />}
+      </Table>
+    </Menus>
+  );
+};
+
+export default BookingTable;
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,12 +1,17 @@
+import BookingTable from '../features/bookings/BookingTable';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 
 const Bookings = () => {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All Bookings</Heading>
-      <p>test paragraph</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All Bookings</Heading>
+        <p>test paragraph</p>
+      </Row>
+
+      <BookingTable />
+    </>
   );
 };
 

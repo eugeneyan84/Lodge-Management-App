d5f4468d27114bddd030b20fe5442bce541ee202
Eugene Yan <enegue84@gmail.com>
2023-12-23 15:10:36 +0800

    check-in button for bookingDetail and bookingRow

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,7 @@ import ButtonText from '../../ui/ButtonText';
 import { useMoveback } from '../../hooks/useMoveback';
 import { useBooking } from './useBooking';
 import Spinner from '../../ui/Spinner';
+import { useNavigate } from 'react-router-dom';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -19,6 +20,7 @@ const HeadingGroup = styled.div`
 `;
 
 const BookingDetail = () => {
+  const navigate = useNavigate();
   const { booking, isLoading } = useBooking();
 
   const moveBack = useMoveback();
@@ -51,6 +53,11 @@ const BookingDetail = () => {
       {<BookingDataBox booking={booking} />}
 
       <ButtonGroup>
+        {status === 'unconfirmed' && (
+          <Button onClick={() => navigate(`checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>

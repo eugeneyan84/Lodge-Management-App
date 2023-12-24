594b342b17e3019123aafcdde6d1db5c92c86da6
Eugene Yan <enegue84@gmail.com>
2023-12-24 14:11:41 +0800

    immeidately set user data in cache so as to avoid calling getCurrentUser redundantly after a successful login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,22 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 import { login as loginApi } from '../../services/apiAuth';
 
 export const useLogin = () => {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isPending: isLoading } = useMutation({
     mutationFn: ({ email, password }) => {
       return loginApi({ email, password });
     },
     onSuccess: (userData) => {
+      // to assist useQuery in useUser, so react-query
+      // can immediately use the data instead of having to
+      // trigger th API call
+      queryClient.setQueriesData(['user'], userData);
+
       navigate('/dashboard');
     },
     onError: (error) => {

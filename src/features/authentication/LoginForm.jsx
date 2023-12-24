ae1ecf90d6d54f9e879204803ea887395238d777
Eugene Yan <enegue84@gmail.com>
2023-12-24 14:14:49 +0800

    clear credential fields in login form after an attempt on logging in

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -17,7 +17,15 @@ const LoginForm = () => {
     if (!email || !password) {
       return;
     } else {
-      login({ email, password });
+      login(
+        { email, password },
+        {
+          onSettled: () => {
+            setEmail('');
+            setPassword('');
+          },
+        }
+      );
     }
   };
 

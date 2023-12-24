393eba1e7225020ded5c00e7bd41e98a803678c0
Eugene Yan <enegue84@gmail.com>
2023-12-24 14:03:09 +0800

    implemented ProtectedRoute component, and use it to wrap AppLayout

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+
+  // 1) load authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2) if no authenticated user, redirect to login page
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  // 3) show spinner while loading #1
+  if (isLoading) {
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  }
+
+  // 4) if there is authenticated user, render app as usual
+  if (isAuthenticated) {
+    return children;
+  }
+};
+
+export default ProtectedRoute;

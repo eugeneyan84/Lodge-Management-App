a56145b1e238745887395855734126d47e9a50c8
Eugene Yan <enegue84@gmail.com>
2023-12-24 14:01:40 +0800

    integrated useLogin hook into LoginForm

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import Logo from '../ui/Logo';
+import Heading from '../ui/Heading';
+import LoginForm from '../features/authentication/LoginForm';
+
 // co-located style
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -12,7 +16,13 @@ const LoginLayout = styled.main`
 `;
 
 const Login = () => {
-  return <LoginLayout>Login</LoginLayout>;
+  return (
+    <LoginLayout>
+      <Logo />
+      <Heading as="h4">Log in to your account</Heading>
+      <LoginForm />
+    </LoginLayout>
+  );
 };
 
 export default Login;

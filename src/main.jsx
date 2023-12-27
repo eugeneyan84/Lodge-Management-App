8523ba081ce8120bd9f812a4476c8c713a0a8291
Eugene Yan <enegue84@gmail.com>
2023-12-27 15:55:37 +0800

    integrate error handling component to capture render errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorHandler from './ui/ErrorHandler.jsx';
+import GlobalStyles from './styles/GlobalStyles.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <GlobalStyles />
+    <ErrorBoundary
+      FallbackComponent={ErrorHandler}
+      onReset={() => window.location.replace('/')}
+    >
+      <App />
+    </ErrorBoundary>
+  </>
+);

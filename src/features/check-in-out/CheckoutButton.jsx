85d04dc670b69289df870a37b44cf92e737d9800
Eugene Yan <enegue84@gmail.com>
2023-12-27 15:34:14 +0800

    checkout button component for dashboard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,20 @@
 import Button from '../../ui/Button';
+import { useCheckout } from './useCheckout';
 
 const CheckoutButton = ({ bookingId }) => {
+  const { checkout, isCheckingOut } = useCheckout();
+
+  const handleCheckout = () => {
+    checkout(bookingId);
+  };
+
   return (
-    <Button $variation="primary" size="small">
+    <Button
+      $variation="primary"
+      size="small"
+      onClick={handleCheckout}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
